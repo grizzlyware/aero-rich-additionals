42b7e7cab68b49f3b01162985b56efa014eb9d9b
Josh Bonfield <josh@grizzlyware.com>
2024-02-02 10:08:03 +0000

    wip: sp

---
+++ src/Providers/RichAdditionalsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Grizzlyware\Aero\RichAdditionals\Providers;
+
+class RichAdditionalsServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
+
